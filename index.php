7e2b3f4b73d485a8cd3c08487947cd1a069b2d40
Shruti Sapre <41475044+sapreshruti@users.noreply.github.com>
2021-07-05 20:35:04 +0530

    Removed unnecessary code

--- index.php
+++ index.php
@@ -11,16 +11,12 @@ require_once 'actions/db_connect.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 4 meta tags *Must* come first in the head; any other head content must come *after* these tags -->
-
     <!-- Title -->
     <title> Home</title>
-
     <!-- Favicon -->
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="style.css">
-
-
 </head>
 
 <body>
